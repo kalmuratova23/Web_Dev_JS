07c2d107b1b23fc6cb7fcd2a471089f65d0f63a5
Akerke <yerlankyzyyy@mail.ru>
2025-02-05 20:33:49 +0500

    slideshow

---
+++ slideshow/slideshow.js
@@ -0,0 +1,15 @@
+const images = [
+    'https://images.pexels.com/photos/39811/pexels-photo-39811.jpeg?cs=srgb&dl=pexels-veeterzy-39811.jpg&fm=jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRf8F7NoP7f81xk4tpYyhZWxbmr3DU7RLslkKsEZnMigE1fW-UCxB01k6l1AxtqzNfJf-I&usqp=CAU',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNz2kdytb3Vz95LRfoJDRRubCoykrS9ueZouLgY10iEAdmxlBqXYDNvlke8l1SSNAkmIA&usqp=CAU',
+    'https://e0.pxfuel.com/wallpapers/108/142/desktop-wallpaper-nature-forest-road-and-background-dark-forest-road.jpg'
+]
+
+function nextSlide(n){
+    console.log(n)
+}
+
+function showSlide(index){
+    
+
+}
